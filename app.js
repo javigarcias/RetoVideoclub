75301eff556619d286df537fd5ee25f1a3f04c4c
javigarcia <javigarciaweb@gmail.com>
2020-10-06 19:17:18 +0200

    created login and signup function in User table

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express'); 
 const filmsRouter = require('./routes/filmRouter');
+const userRouter = require('./routes/userRouter');
 const app = express();
 const PORT = 3000;
 
 
-app.listen(PORT, () => console.log("Server is running..."));
 
 app.use(express.json());
 app.use('/films',filmsRouter);
+app.use('/users',userRouter);
 
+app.listen(PORT, () => console.log("Server is running..."));
---
+++ controllers/UserController.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const {
+    User
+} = require('../models');
+
+const UserController = {
+    async signup(req, res) {
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 9)
+            const user = await User.create(req.body);
+            res.status(201).send(user)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                error,
+                message: 'There was a problem trying to register the user'
+            })
+        }
+    },
+    async login(req, res) {
+        try {
+            const user = await User.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            if (!user) {
+                return res.status(400).send({
+                    message: 'Wrong credentials'
+                })
+            }
+            const isMatch = await bcrypt.compare(req.body.password, user.password)
+            if (!isMatch) {
+                return res.status(400).send({
+                    message: 'Wrong credentials'
+                })
+            }
+            const token = jwt.sign({ id: user.id }, 'pericodelospalotes', { expiresIn: '15d' });
+            console.log(token)
+            user.token = token; 
+            await user.save() 
+            res.send(user);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: 'There was a problem trying to login' })
+        }
+
+    }
+}
+
+module.exports = UserController;
