eaa92ffdb615b612391c30f251e324383df4ed64
javigarcia <javigarciaweb@gmail.com>
2020-10-07 17:27:25 +0200

    Change secret auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const {
 const auth = async(req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, 'mimamamemima');
+        jwt.verify(token, 'pericodelospalotes');
         const user = await User.findOne({ where: { token: token } });
         if (!user) {
             return res.status(401).send({ message: 'You are not authorized' })
